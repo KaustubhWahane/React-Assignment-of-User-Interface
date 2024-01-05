dc7731955e18c2fdcefe10379e31e65bda75eb8e
Lavda <kaustubhwahane55@gmail.com>
2024-01-05 16:19:40 +0530

    Mistakes in Changing to another state for showing full data

---
+++ userinterface/src/components/UserDetail.jsx
@@ -0,0 +1,18 @@
+// import UserList from './UserList';
+
+// export default function UserDetail({all_data}) {
+
+//   return (
+//     <>
+//     <div>
+//       <ul className='displayData'>
+//         {all_data.map((item) => (
+//           <button key={item.id}>{item.name}</button>
+//         ))}
+//       </ul>
+//       <UserList user_data={all_data} />
+//     </div>
+//     </>
+//   );
+ 
+// }
--- userinterface/src/components/UserList.jsx
+++ userinterface/src/components/UserList.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import '../App.css'
-export default function UserList({ user_data }) {
+import '../App.css';
+
+export default function UserList({ user_data  }) {
+//   const [all_data , setAll_data] = useState([]);
+
+// const data = async() => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const result = await res.json();
+//     setAll_data(result);
+//     console.log(result)
+// }
+// useEffect(() => {
+//     data();
+//   }, []);
+
   return (
     <div>
       <ul className='displayData'>
         {user_data.map((item) => (
-          <button>{item.name}</button>
-        ))}
-      </ul>
-      <ul className='regularData'>
-        {user_data.map((item) => (
-          <h3>{item.email}</h3>
+          <button key={item.id}>{item.name}</button>
         ))}
       </ul>
     </div>
